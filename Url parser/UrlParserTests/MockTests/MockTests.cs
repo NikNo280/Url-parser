7e7743b2c9576faf5bb6f87414bedd4faa911cc1
Никита Сидоренко <nikitastar280@gmail.com>
2021-04-14 00:15:28 +0300

    add mock

---
+++ Url parser/UrlParserTests/MockTests/MockTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Url_parser;
+using Url_parser.Writer;
+
+namespace UrlParserTests.MockTests
+{
+    public class MockTests
+    {
+        [Test]
+        public void TestWrite()
+        {
+            var list = new List<UrlModel>();
+            var mock = new Mock<IWriter<UrlModel>>();
+            mock.Setup(obj => obj.Write(list.ToArray(), "test.xml"));
+            mock.Verify();
+        }
+    }
+}
